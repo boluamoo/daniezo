2262a6fb7283fe406ee3d2d0d37189bf2936f3c1
boluamoo <boluamoo58@gmail.com>
2024-03-06 21:07:48 +0100

    correct mobile navigation

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const navOpen = document.getElementById('open')
-const navClose = document.getElementById('close')
-const navLinks = document.getElementById('nav-links')
-const navServices = document.querySelector('.nav-services')
+// const navOpen = document.getElementById('open')
+// const navClose = document.getElementById('close')
+// const navLinks = document.getElementById('nav-links')
+// const navServices = document.querySelector('.nav-services')
 
-navOpen.addEventListener('click', () => {
-  navLinks.style.bottom = 0
-})
-navClose.addEventListener('click', () => {
-  navLinks.style.bottom = '100%'
-})
-navServices.addEventListener('click', () => {
-  navLinks.style.bottom = '100%'
-})
+// navOpen.addEventListener('click', () => {
+//   navLinks.style.bottom = 0
+// })
+// navClose.addEventListener('click', () => {
+//   navLinks.style.bottom = '100%'
+// })
+// navServices.addEventListener('click', () => {
+//   navLinks.style.bottom = '100%'
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.querySelector('.menu-open');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const navServices = document.querySelector('.nav-services');
+
+  menuIcon.addEventListener('click', function () {
+    mobileNav.classList.toggle('open');
+    menuIcon.classList.toggle('close');
+  });
+  navServices.addEventListener('click', function () {
+    mobileNav.classList.toggle('open');
+    menuIcon.classList.toggle('close');
+  });
+});
